99b3212f2f4429af0893c6643570651451afb20f
José Luis López Sánchez <selutario@gmail.com>
2018-11-09 23:22:52 +0100

    Añadida clave OWM

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ import pyowm
 import os
 
 app = Flask(__name__)
-owmapikey=os.environ.get('OWMApiKey') #or provide your key here
+owmapikey=38102aad5ca87e660ced55b1aba89c77 #or provide your key here
 owm = pyowm.OWM(owmapikey)
 
 #geting and sending response to dialogflow
